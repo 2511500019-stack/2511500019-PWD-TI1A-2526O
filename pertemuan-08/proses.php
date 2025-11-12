e4260d05e045df70b3b7bffd04fea142ab3fe6a6
Nicolas Kalim <2511500019@mahasiswa.atmaluhur.ac.id>
2025-11-12 11:53:50 +0700

    update lat1.php

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,10 +1,14 @@
 <?php
 session_start();
-$sesnama = $_POST["txtNama"];
-$sesemail = $_POST["txtEmail"];
-$sespesan = $_POST["txtPesan"];
-$_SESSION["sesnama"] = $sesnama;
-$_SESSION["sesemail"] = $sesemail;
-$_SESSION["sespesan"] = $sespesan;
-header("location: index.php");
+<h3>Data Profil Pengunjung (Dari Form)</h3>
+    <p><strong>NIM:</strong> <?= $_SESSION["nim"]; ?></p>
+    <p><strong>Nama Lengkap:</strong> <?= $_SESSION["namaLengkap"]; ?></p>
+    <p><strong>Tempat Lahir:</strong> <?= $_SESSION["tempatLahir"]; ?></p>
+    <p><strong>Tanggal Lahir:</strong> <?= $_SESSION["tanggalLahir"]; ?></p>
+    <p><strong>Hobi:</strong> <?= $_SESSION["hobi"]; ?></p>
+    <p><strong>Pasangan:</strong> <?= $_SESSION["pasangan"]; ?></p>
+    <p><strong>Pekerjaan:</strong> <?= $_SESSION["pekerjaan"]; ?></p>
+    <p><strong>Nama Orang Tua:</strong> <?= $_SESSION["orangTua"]; ?></p>
+    <p><strong>Nama Kakak:</strong> <?= $_SESSION["kakak"]; ?></p>
+    <p><strong>Nama Adik:</strong> <?= $_SESSION["adik"]; ?></p>
 ?>
