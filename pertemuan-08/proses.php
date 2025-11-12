4327d2db79052f471e4d8c722842020b5dc4ee35
Nicolas Kalim <2511500019@mahasiswa.atmaluhur.ac.id>
2025-11-12 11:55:52 +0700

    update lat1.php

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -1,15 +1,3 @@
-<section id="about">
-  <h2>Tentang Saya</h2>
-  <p><strong>NIM:</strong> 0344300002</p>
-  <p><strong>Nama Lengkap:</strong> Yohanes Setiawan Japriadi</p>
-  <p><strong>Tempat Lahir:</strong> Pangkalpinang</p>
-  <p><strong>Tanggal Lahir:</strong> 1 Januari 2000</p>
-  <p><strong>Hobi:</strong> Coding dan musik</p>
-  <p><strong>Pasangan:</strong> Belum ada</p>
-  <p><strong>Pekerjaan:</strong> Mahasiswa</p>
-  <p><strong>Nama Orang Tua:</strong> Bapak Setiawan & Ibu Maria</p>
-  <p><strong>Nama Kakak:</strong> Antonius Setiawan</p>
-  <p><strong>Nama Adik:</strong> Beni Setiawan</p>
 <?php
 session_start();
 <h3>Data Profil Pengunjung (Dari Form)</h3>
@@ -24,4 +12,3 @@ session_start();
     <p><strong>Nama Kakak:</strong> <?= $_SESSION["kakak"]; ?></p>
     <p><strong>Nama Adik:</strong> <?= $_SESSION["adik"]; ?></p>
 ?>
-</section>
