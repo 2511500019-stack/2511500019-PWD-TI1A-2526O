69af7f940f3289c29ec249b61587fdd235e13d38
Nicolas Kalim <2511500019@mahasiswa.atmaluhur.ac.id>
2025-11-12 12:06:16 +0700

    update lat1.php

--- pertemuan-08/proses.php
+++ pertemuan-08/proses.php
@@ -16,14 +16,15 @@
 <?php
 session_start();
 <h3>Data Profil Pengunjung (Dari Form)</h3>
-    <p><strong>NIM:</strong> <?= $_SESSION["nim"]; ?></p>
-    <p><strong>Nama Lengkap:</strong> <?= $_SESSION["namaLengkap"]; ?></p>
-    <p><strong>Tempat Lahir:</strong> <?= $_SESSION["tempatLahir"]; ?></p>
-    <p><strong>Tanggal Lahir:</strong> <?= $_SESSION["tanggalLahir"]; ?></p>
-    <p><strong>Hobi:</strong> <?= $_SESSION["hobi"]; ?></p>
-    <p><strong>Pasangan:</strong> <?= $_SESSION["pasangan"]; ?></p>
-    <p><strong>Pekerjaan:</strong> <?= $_SESSION["pekerjaan"]; ?></p>
-    <p><strong>Nama Orang Tua:</strong> <?= $_SESSION["orangTua"]; ?></p>
-    <p><strong>Nama Kakak:</strong> <?= $_SESSION["kakak"]; ?></p>
-    <p><strong>Nama Adik:</strong> <?= $_SESSION["adik"]; ?></p>
+    $sesnim = $_SESSION["nim"]; 
+    $sesnamalengkap = $_SESSION["namaLengkap"]; 
+    $sestanggallahir = $_SESSION["tempatLahir"]; 
+    $sestanggallahir = $_SESSION["tanggalLahir"];
+    $seshobi = $_SESSION["hobi"]; 
+    $sespasangan = $_SESSION["pasangan"]; 
+    $sespekerjan = $_SESSION["pekerjaan"]; 
+    $sesorangtua = $_SESSION["orangTua"]; 
+    $seskakak = $_SESSION["kakak"]; 
+    $sesadik = $_SESSION["adik"]; 
 ?>
+
