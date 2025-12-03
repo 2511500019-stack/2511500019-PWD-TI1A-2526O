f8599c391b81eecf9fdaf6d16a5914425ec3b317
Nicolas Kalim <2511500019@mahasiswa.atmaluhur.ac.id>
2025-12-03 12:33:23 +0700

    update index.php

--- pertemuan-10/read.php
+++ pertemuan-10/read.php
@@ -1,23 +1,23 @@
 <?php
-require_once "koneksi.php";
+require 'koneksi.php';
 
-$sql = "SELECT * FROM tbl_tamu ORDER BY id DESC";
+$sql = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
 $q = mysqli_query($conn, $sql);
 ?>
 <table border="1" cellpadding="8" cellspacing="0">
+  <tr>
+    <th>ID</th>
+    <th>Nama</th>
+    <th>Email</th>
+    <th>Pesan</th>
+  </tr>
+
+  <?php while ($row = mysqli_fetch_assoc($q)): ?>
     <tr>
-      <th>ID</th>
-      <th>Nama</th>
-      <th>Email</th>
-      <th>Pesan</th>
+    <td><?= $row['cid']; ?></td>
+    <td><?=htmlspecialchars($row['cnama']); ?></td>
+    <td><?=htmlspecialchars($row['cemail']); ?></td>
+    <td><?=nl2br(htmlspecialchars($row['cpesan'])); ?></td>
     </tr>
-  
-    <?php while ($row = mysqli_fetch_assoc($q)) : ?>
-      <tr>
-        <td><?php echo htmlspecialchars($row['id']); ?></td>
-        <td><?php echo htmlspecialchars($row['nama']); ?></td>
-        <td><?php echo htmlspecialchars($row['email']); ?></td>
-        <td><?php echo htmlspecialchars($row['pesan']); ?></td>
-      </tr>
     <?php endwhile; ?>
-  </tbody>
+</table>
