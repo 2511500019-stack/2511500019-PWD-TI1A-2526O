0952164d70f70eb2e555f10d12be435571d2dc41
Nicolas Kalim <2511500019@mahasiswa.atmaluhur.ac.id>
2025-12-06 13:54:44 +0700

    update index.php

---
+++ pertemuan-10/localhost.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Waktu pembuatan: 03 Des 2025 pada 05.38
+-- Versi server: 5.7.33
+-- Versi PHP: 7.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_pwd2025`
+--
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+USE `db_pwd2025`;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_tamu`
+--
+
+CREATE TABLE `tbl_tamu` (
+  `cid` int(11) NOT NULL,
+  `cnama` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `cemail` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `cpesan` text COLLATE utf8mb4_bin
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data untuk tabel `tbl_tamu`
+--
+
+INSERT INTO `tbl_tamu` (`cid`, `cnama`, `cemail`, `cpesan`) VALUES
+(1, 'Nicolas Kalim', '2511500019@mahasiswa.atmaluhur.ac.id', 'Ayo yang teliti belajar pemograman web dasarnya, jangan membiasakan typo'),
+(2, 'ejpapinka', 'erwinccg@gmail.com', 'bonghew adalah orang faforite ku'),
+(3, 'wanyu', 'baserccg@gmail.com', 'balui adalah orang fofirite ku');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tbl_tamu`
+--
+ALTER TABLE `tbl_tamu`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
